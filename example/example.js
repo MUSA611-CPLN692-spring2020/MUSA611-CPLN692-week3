89d8dd07478f1cd3f038bcee0b83838ef70457ec
liziqun <492532763@qq.com>
2020-02-07 14:13:23 -0500

    update

--- example/example.js
+++ example/example.js
@@ -12,6 +12,18 @@
     ext: 'png'
   }).addTo(map);
 
+  /* =====================
+
+  # Lab 2, Part 3
+
+  ## Introduction
+
+    You've already seen this file organized and refactored. In this lab, you will
+    try to refactor this code to be cleaner and clearer - you should use the
+    utilities and functions provided by underscore.js. Eliminate loops where possible.
+
+  ===================== */
+
   // Mock user input
   // Filter out according to these zip codes:
   var acceptedZipcodes = [19106, 19107, 19124, 19111, 19118];
@@ -20,6 +32,7 @@
 
 
   // clean the data
+  /*
   for (var i = 0; i < schools.length - 1; i++) {
     // If we have '19104 - 1234', splitting and taking the first (0th) element
     // as an integer should yield a zip in the format above
@@ -28,9 +41,16 @@
       var normalized_zip = parseInt(split[0]);
       schools[i].ZIPCODE = normalized_zip;
     }
+    */
+    _.each(schools,function(x){
+      if(typeof x.ZIPCODE === 'string') {
+        _.first(x.ZIPCODE.split(' '));
+    }
+  })
 
     // Check out the use of typeof here — this was not a contrived example.
     // Someone actually messed up the data entry.
+    /*
     if (typeof schools[i].GRADE_ORG === 'number') {
       schools[i].HAS_KINDERGARTEN = schools[i].GRADE_LEVEL < 1;
       schools[i].HAS_ELEMENTARY = 1 < schools[i].GRADE_LEVEL < 6;
@@ -43,10 +63,25 @@
       schools[i].HAS_HIGH_SCHOOL = schools[i].GRADE_LEVEL.toUpperCase().indexOf('HIGH') >= 0;
     }
   }
+*/
+  _.each(schools,function(y){
+    if (typeof y.GRADE_ORG === 'number') {
+      y.HAS_KINDERGARTEN = y.GRADE_LEVEL < 1;
+      y.HAS_ELEMENTARY = 1 < y.GRADE_LEVEL < 6;
+      y.HAS_MIDDLE_SCHOOL = 5 < y.GRADE_LEVEL < 9;
+      y.HAS_HIGH_SCHOOL = 8 < y.GRADE_LEVEL < 13;
+    } else {
+      y.HAS_KINDERGARTEN = y.GRADE_LEVEL.toUpperCase().indexOf('K') >= 0;
+      y.HAS_ELEMENTARY = y.GRADE_LEVEL.toUpperCase().indexOf('ELEM') >= 0;
+      y.HAS_MIDDLE_SCHOOL = y.GRADE_LEVEL.toUpperCase().indexOf('MID') >= 0;
+      y.HAS_HIGH_SCHOOL = y.GRADE_LEVEL.toUpperCase().indexOf('HIGH') >= 0;
+  }
+})
 
   // filter data
   var filtered_data = [];
   var filtered_out = [];
+  /*
   for (var i = 0; i < schools.length - 1; i++) {
     // These really should be predicates!
     isOpen = schools[i].ACTIVE.toUpperCase() == 'OPEN';
@@ -69,11 +104,35 @@
       filtered_out.push(schools[i]);
     }
   }
+*/
+  _.each(schools,function(z){
+    isOpen = z.ACTIVE.toUpperCase() == 'OPEN';
+    isPublic = (z.TYPE.toUpperCase() !== 'CHARTER' ||
+                z.TYPE.toUpperCase() !== 'PRIVATE');
+    isSchool = (z.HAS_KINDERGARTEN ||
+                z.HAS_ELEMENTARY ||
+                z.HAS_MIDDLE_SCHOOL ||
+                z.HAS_HIGH_SCHOOL);
+    meetsMinimumEnrollment = z.ENROLLMENT > minEnrollment;
+    meetsZipCondition = acceptedZipcodes.indexOf(z.ZIPCODE) >= 0;
+    filter_condition = (isOpen &&
+                        isSchool &&
+                        meetsMinimumEnrollment &&
+                        !meetsZipCondition);
+
+    if (filter_condition) {
+      filtered_data.push(z);
+    } else {
+      filtered_out.push(z);
+    }
+  })
+
   console.log('Included:', filtered_data.length);
   console.log('Excluded:', filtered_out.length);
 
   // main loop
   var color;
+  /*
   for (var i = 0; i < filtered_data.length - 1; i++) {
     isOpen = filtered_data[i].ACTIVE.toUpperCase() == 'OPEN';
     isPublic = (filtered_data[i].TYPE.toUpperCase() !== 'CHARTER' ||
@@ -96,5 +155,28 @@
       .bindPopup(filtered_data[i].FACILNAME_LABEL)
       .addTo(map);
   }
+*/
+  _.each(filtered_data,function(i){
+    isOpen = i.ACTIVE.toUpperCase() == 'OPEN';
+    isPublic = (i.TYPE.toUpperCase() !== 'CHARTER' ||
+                i.TYPE.toUpperCase() !== 'PRIVATE');
+    meetsMinimumEnrollment = i.ENROLLMENT > minEnrollment;
+
+    // Constructing the styling  options for our map
+    if (i.HAS_HIGH_SCHOOL){
+      color = '#0000FF'; // blue
+    } else if (i.HAS_MIDDLE_SCHOOL) {
+      color = '#00FF00'; // green
+    } else {
+      color = '#FF0000'; //red
+    }
+
+    // The style options - note that we're using an object to define properties
+    var pathOpts = {'radius': i.ENROLLMENT / 30,
+                    'fillColor': color};
+    L.circleMarker([i.Y, i.X], pathOpts)
+      .bindPopup(i.FACILNAME_LABEL)
+      .addTo(map);
+  })
 
 })();
