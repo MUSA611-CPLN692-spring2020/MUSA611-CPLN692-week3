4ef4cbc1146af72e236a089154bd614f3b6effef
HanyongXu <xuhanyon@upenn.edu>
2020-02-07 01:21:30 -0500

    HW3
    hw 3 by hanyong

--- lab/lab1/part2.js
+++ lab/lab1/part2.js
@@ -7,7 +7,9 @@ Functions that `return` can be passed as values to other functions. Each exercis
 Instructions: Write a function that *always* returns the number 1.
 ===================== */
 
-var justOne = function() {};
+var justOne = function() {
+  return 1;
+};
 
 console.log('justOne success:', justOne() === 1);
 
@@ -15,7 +17,13 @@ console.log('justOne success:', justOne() === 1);
 Instructions: Write a function that returns true if a number is even.
 ===================== */
 
-var isEven = function() {};
+var isEven = function(number) {
+  if (number%2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 console.log('isEven success:', isEven(2) === true && isEven(3) === false);
 
@@ -24,7 +32,9 @@ Instructions: Write a function that *always* returns false.
               Use functions "justOne" and "isEven" somehow in the definition.
 ===================== */
 
-var justFalse = function() {};
+var justFalse = function() {
+  return isEven(justOne());
+};
 
 console.log('justFalse success:', justFalse() === false);
 
@@ -32,7 +42,9 @@ console.log('justFalse success:', justFalse() === false);
 Instructions: Write a function that takes a boolean value and returns its opposite.
 ===================== */
 
-var not = function() {};
+var not = function(boo) {
+  return !boo;
+};
 
 console.log('not success:', not(true) === false);
 
@@ -41,7 +53,9 @@ Instructions: Write a function that returns true if a number is odd
               Use functions "isEven" and "not" somehow in the definition.
 ===================== */
 
-var isOdd = function() {};
+var isOdd = function(num) {
+  return not(isEven(num));
+};
 
 console.log('isOdd success:', isOdd(4) === false);
 
@@ -49,12 +63,33 @@ console.log('isOdd success:', isOdd(4) === false);
 Instructions: Write a function that takes a list of numbers and returns a list with only numbers above 10
 ===================== */
 
-var filterOutLessThan10 = function() {};
+var filterOutLessThan10 = function(list) {
+  var output = [];
+  for (i = 0; i < list.length; i++) {
+    if (list[i] > 10) {
+      output.push(list[i]);
+    }
+  }
+  return output;
+};
+
+var arraysEqual = function(a, b) {
+  //if (a === b) return true;
+  //if (a == null || b == null) return false;
+  if (a.length != b.length) {
+    for (i = 0; i < a.length; i ++) {
+      if (a[i] !== b[i]) {
+        return false;
+      }
+    }
+  } else {
+    return true;
+  }
+};
 // The function 'arraysEqual' (which it is your task to define) is necessary because
 // ([4] === [4]) is *false* in javascript(!!!)
 // Use google + stackoverflow to figure out how to define a function which returns true given two equal arrays
-function arraysEqual(arr1, arr2) { return false; }
-console.log('filterOutLessThan10 success:', arraysEqual([4, 11], [11]));
+console.log('filterOutLessThan10 success:', arraysEqual(filterOutLessThan10([4, 11]), [11]));
 
 /* =====================
 Stretch goal
@@ -63,7 +98,14 @@ Instructions: Let's bring it all together. Write a function that filters a list 
               2. a function that takes a value and returns true (to keep a number) or false (to toss it out)
 ===================== */
 
-var filter = function(array, func) {};
+var filter = function(array, func) {
+  var list = [];
+  for (i = 0; i < array.length; i ++) {
+    if (func(array[i])) {
+      list.push(array[i]);
+    }
+  }
+  return list;
+};
 
-console.log('filter success:', filter([4, 11], isOdd) === [11]);
-
+console.log('filter success:', arraysEqual(filter([4, 11], isOdd), [11]));
